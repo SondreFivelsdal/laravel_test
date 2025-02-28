572ed7fa6409b4ba486315bc6ba2d149eca320a7
sondrefivelsdal <sondrebfivelsdal@gmail.com>
2025-02-28 21:24:14 +0100

    login/logout page added(no code)

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/index', 'index')->name('dashboard.index');
+Route::post('/index', 'index')->name('dashboard.index.post');
+
+Route::get('/login', 'login')->name('login');
+Route::post('/login', 'login')->name('login.post');
+
+Route::get('/register', 'register')->name('register');
+Route::post('/register', 'register')->name('register.post');
+
+Route::get('/forgot-password', 'forgot-password')->name('forgot-password');
+Route::post('/forgot-password', 'forgot-password')->name('forgot-password.post');
+
+Route::get('/logout', 'logout')->name('logout');
